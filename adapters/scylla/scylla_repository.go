b964ed19591f69bf9d9cd5079ec0cb54d55189d8
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2024-02-12 21:57:42 +0100

    Implement GetStatement in Scylla repository

--- adapters/scylla/scylla_repository.go
+++ adapters/scylla/scylla_repository.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gocql/gocql"
 	"github.com/rivelles/rinha-backend-2024-q1/model"
+	"log"
 )
 
 type ScyllaRepository struct {
@@ -22,8 +23,30 @@ func (s ScyllaRepository) SaveTransaction(transaction model.Transaction) {
 }
 
 func (s ScyllaRepository) GetStatement(clientId int) model.Statement {
-	return model.Statement{
-		ClientId:     clientId,
-		Transactions: make([]model.Transaction, 0),
+	scanner := s.session.Query(`SELECT * FROM transactions WHERE client_id = ?`,
+		clientId).Iter().Scanner()
+	var transactions []model.Transaction
+	for scanner.Next() {
+		var (
+			value           int
+			transactionType string
+			description     string
+		)
+		err := scanner.Scan(&value, &transactionType, &description)
+		if err != nil {
+			log.Fatal(err)
+		}
+		i := append(transactions, model.Transaction{
+			clientId,
+			value,
+			transactionType,
+			description,
+		})
+		transactions = i
 	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return model.Statement{ClientId: clientId, Transactions: transactions}
 }
